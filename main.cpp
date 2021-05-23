6b99be783a2bd6ac47ead43cde896551e5c5428a
Saulo Gelinski <>
2021-05-23 04:47:58 +0000

    ultima mexida

--- main.cpp
+++ main.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
- 
- using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+ using namespace std;
+int fazendo_troca(int tam, string *pontMsg, string *pontMsg2, int letra1, int letra2){
+
+    int troca = 0;
+
+     for (int i=0; i < tam; i++)
+       {
+         if ((*pontMsg)[i]==letra1)
+         {
+          (*pontMsg2)[i] = letra2;
+           troca++;
+           cout << troca <<endl;
+         }
+
+       }
+
+    return troca;
+}
 int main() {
 
-string msg;
-string *pontMsg;
-char l1;
-char l2;
 
-cout << "Digite a mensagem" << endl;
- cin >> msg;
+    string msg, msg2;
+    string *pontMsg = &msg;
+    string *pontMsg2 = &msg2;
 
- cout << "Letra que será substituida" << endl;
-  cin >> l1;
+    char letra1;
+    char letra2;
 
-  cout << "Letra que vai no lugar da original" << endl;
-   cin >> l2;
+    cout << "Digite a mensagem" << endl;
+    cin >> msg;
+    msg2 = msg;
 
-  cout << msg << endl;
-  cout << l1 << endl;
-  cout << l2 << endl;
+    cout << "Letra que sera substituida" << endl;
+    cin >> letra1;
+
+    cout << "Letra que vai no lugar da original" << endl;
+    cin >> letra2;
+
+
+    int tam = msg.size();
+
+    int qtdTrocas = fazendo_troca(tam, pontMsg, pontMsg2, letra1, letra2);
+
+
+    /*cout << "Entrada" << endl;
+    cout << k1 << endl;
+    cout << l1 << endl;
+    cout << l2 << endl;*/
+    cout << "Saida" << endl;
+    cout << msg2 << endl;
+    cout << msg << endl;
+    cout << qtdTrocas;
 }
